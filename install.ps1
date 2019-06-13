92ba1bc0075787427be9704d83bc5ba3c63bae26
Stellan Eriksson <50230797+Swedeniron@users.noreply.github.com>
2019-06-13 15:10:06 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,62 +2,10 @@
 # Install script for Azure namespaces                                                                     #
 # In future this can be used as a ground for more automatical deployments for az commands (Azures cli)    #
 #                                                                                                         #
-# Maintainer Stellan Eriksson micro_se@hotmail.com                                                        #
+# Maintainer Stellan Eriksson stellan.eriksson@tele2.com                                                  #
 # Written    2019-06-12                                                                                   #
 #                                                                                                         #
 ###########################################################################################################
-
-Write-Host "Deploy a namespace to Azure"
-Write-Host ' '
-
-# First we need to login to Azure with your AD account!"
-$login = 'First we need to login to Azure with your AD account! [Y/N]'
-do {
-    $response_login = Read-Host -Prompt $login
-    if ($response_login -eq 'y') {
-		az login
-		Write-Host ' '
-	}	
-} until ($response_login -eq 'n')
-
-
-#List Available account
-Write-Host ' '
-Write-Host "Available account"
-Write-Host ' '
-az account list --query '[].{name:name}' --output table
-Write-Host ' '
-
-#Use the right account"
-$account = Read-Host -Prompt 'Please enter the "Name" of the account you want to use and press [ENTER] '
-Write-Host ' '
-
-#Set the account
-az account set --subscription $account
-Write-Host ' '
-
-#List the account
-az account list --query '[].{Name:name, IsDefault:isDefault}' --output table | Select-String -Pattern 'True' -CaseSensitive -SimpleMatch
-Write-Host ' '
-
-$msg = 'Right account? [Y/N]'
-do {
-    $response = Read-Host -Prompt $msg
-    if ($response -eq 'n') {
-        
-		
-		Write-Host 'We try again..... '
-		Write-Host ' '
-		az account list --query '[].{name:name}' --output table
-		Write-Host ' '
-		$account = Read-Host -Prompt 'Please enter the "Name" of the account you want to use and press [ENTER] '
-		Write-Host ' '
-		az account set --subscription $account
-		Write-Host ' '
-		az account list --query '[].{Name:name, IsDefault:isDefault}' --output table | Select-String -Pattern 'True' -CaseSensitive -SimpleMatch
-    }
-} until ($response -eq 'y')
-
 
 
 #################################################################
@@ -65,11 +13,18 @@ do {
 # Was planing to put a menu here to do more Azure deployments
 # Make include of this type of scripts
 #
-# 1. Adding teams to the project
-# 2. Adding VSTS groups
-# 3. Adding a Git Repo
-# 4. Adding security to groups within the project
+# 1: Login to you Azure account. WORKING
+# 2: Change tho another Azure account. WORKING
+# 3: Adding namespace. WORKING
+# 4: Adding a project. NOT WORKING
+# 5: Adding teams to the project. NOT WORKING
+# 6: Adding VSTS groups. NOT WORKING
+# 7: Adding a Git Repository. NOT WORKING
+# 8: Adding security to groups within the project. NOT WORKING
+# Q: Press 'Q' to quit. WORKING
 #
+
+
 #
 # Include required files 
 #
@@ -90,17 +45,20 @@ catch {
 function Show-Menu
 {
     param (
-        [string]$Title = 'Menu'
+        [string]$Title = 'Azure deploy Menu'
     )
     Clear-Host
     Write-Host "================ $Title ================"
     
-    Write-Host "1: Adding namespace."
-	Write-Host "2: Adding teams to the project."
-    Write-Host "3: Adding VSTS groups."
-    Write-Host "4: Adding a Git Repository."
-	Write-Host "5: Adding security to groups within the project."
-    Write-Host "Q: Press 'Q' to quit."
+    Write-Host "1: Login to you Azure account." -ForegroundColor Green
+	Write-Host "2: Change tho another Azure account." -ForegroundColor Green
+	Write-Host "3: Adding namespace." -ForegroundColor Green
+	Write-Host "4: Adding a project." -ForegroundColor Red
+	Write-Host "5: Adding teams to the project." -ForegroundColor Red
+    Write-Host "6: Adding VSTS groups." -ForegroundColor Red
+    Write-Host "7: Adding a Git Repository." -ForegroundColor Red
+	Write-Host "8: Adding security to groups within the project." -ForegroundColor Red
+    Write-Host "Q: Press 'Q' to quit."  -ForegroundColor White
 }
 
 	
@@ -110,7 +68,68 @@ do
      $input = Read-Host "Please make a selection" 
      switch ($input) 
      { 
-           '1' { 
+	 
+			 '1' {
+			 cls 
+             'Login to Azure with your AD account!'
+			
+			# First we need to login to Azure with your AD account!"
+				#$login = 'First we need to login to Azure with your AD account! [Y/N]'
+				#do {
+				#$response_login = Read-Host -Prompt $login
+				#if ($response_login -eq 'y') {
+				#Write-Host 'First we need to login to Azure with your AD account!'
+				az login
+				#Write-Host ' '
+				#}	
+			    #} until ($response_login -eq 'n')
+	 
+	 
+	 
+	 		}	'2' { 
+                cls 
+                'Change tho another Azure account' 	
+
+				#List Available account
+				Write-Host ' '
+				Write-Host "Available account"
+				Write-Host ' '
+				az account list --query '[].{name:name}' --output table
+				Write-Host ' '
+
+				#Use the right account"
+				$account = Read-Host -Prompt 'Please enter the "Name" of the account you want to use and press [ENTER] '
+				Write-Host ' '
+
+				#Set the account
+				az account set --subscription $account
+				Write-Host ' '
+				
+				#List the account
+                az account list --query '[].{Name:name, IsDefault:isDefault}' --output table | Select-String -Pattern 'True' -CaseSensitive -SimpleMatch
+                Write-Host ' '
+
+                $msg = 'Right account? [Y/N]'
+                do {
+                    $response = Read-Host -Prompt $msg
+                    if ($response -eq 'n') {
+        
+		
+                		Write-Host 'We try again..... '
+	                	Write-Host ' '
+	                	az account list --query '[].{name:name}' --output table
+	                	Write-Host ' '
+	                	$account = Read-Host -Prompt 'Please enter the "Name" of the account you want to use and press [ENTER] '
+	                	Write-Host ' '
+		                az account set --subscription $account
+	                	Write-Host ' '
+	                	az account list --query '[].{Name:name, IsDefault:isDefault}' --output table | Select-String -Pattern 'True' -CaseSensitive -SimpleMatch
+						Write-Host ' '
+                    }
+                } until ($response -eq 'y')
+				
+				
+		} '3' { 
                 cls 
                 'Adding namespace'
 				
@@ -209,19 +228,24 @@ do
 
 
 
-				
-           } '2' { 
+		   } '4' { 
+                cls 
+                'Adding a project' 	
+           } '5' { 
                 cls 
                 'Adding teams to the project' 
-           } '3' { 
+           } '6' { 
                 cls 
                 'Adding VSTS groups' 
-			} '4' { 
+			} '7' { 
                 cls 
                 'Adding a Git Repository' 
-			} '5' { 
+			} '8' { 
                 cls 
                 'Adding security to groups within the project' 	
+			} '9' { 
+                cls 
+                'Adding security to groups within the project'
 			} 'q' { 
                 return 
            } 
