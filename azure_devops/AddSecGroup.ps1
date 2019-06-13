d8f9596bc8bf19953f69ad4c0b7cc0fdb7aca5ec
Stellan Eriksson <50230797+Swedeniron@users.noreply.github.com>
2019-06-13 13:40:26 +0200

    Added some include scripts to the future menu

---
+++ azure_devops/AddSecGroup.ps1
@@ -0,0 +1,10 @@
+###########################################################################################################
+# Install script for Azure namespaces                                                                     #
+# In future this can be used as a ground for more automatical deployments for az commands (Azures cli)    #
+#                                                                                                         #
+# Maintainer Stellan Eriksson micro_se@hotmail.com                                                  #
+# Written    2019-06-12                                                                                   #
+#                                                                                                         #
+###########################################################################################################
+
+Write-Host "Add Secure group to to Azure"
---
+++ azure_devops/AddTeam.ps1
@@ -0,0 +1,10 @@
+###########################################################################################################
+# Install script for Azure namespaces                                                                     #
+# In future this can be used as a ground for more automatical deployments for az commands (Azures cli)    #
+#                                                                                                         #
+# Maintainer Stellan Eriksson micro_se@hotmail.com                                                  #
+# Written    2019-06-12                                                                                   #
+#                                                                                                         #
+###########################################################################################################
+
+# Write-Host "Add Teams to to Azure"
